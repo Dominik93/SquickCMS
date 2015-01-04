78aa40158099f194b3b179867735d32646f3835d
Dominik Ślusarz <slusarz.dominik@gmail.com>
2015-01-04 22:30:41 +0100

    to co mówiłem

--- Classes/Backup.php
+++ Classes/Backup.php
@@ -5,7 +5,8 @@ class Backup{
     private $dataBase;
     private $userName;
     private $password;
-    private $arrayTable;
+    private $tables;
+    private $views;
     /*
      * Konstruktor podanie scieżki w której będą zapisywane backupy, nazwy bazy, oraz użytkownika i hasła do bazy
      * autopatycznie przypisuje do obiektu wszytkie tabele w bazie
@@ -15,7 +16,9 @@ class Backup{
         $this->dataBase = $dataBase;
         $this->userName = $userName;
         $this->password = $password;
-        $this->arrayTable = array("acces_rights", "admins", "authors", "authors_books", "books", "borrows", "logs_readers", "news", "publisher_houses", "readers", "sessions");
+        $this->tables = array("acces_rights", "admins", "authors", "authors_books", "books", "borrows", "logs_readers", "news", "publisher_houses", "readers", "sessions");
+        $this->views = array();
+        
     }
     
     public function toString(){
@@ -23,73 +26,11 @@ class Backup{
     }
 
     /*
-     * jeżeli to cholerstwo myslqdump nie będzie działac to tu jest coś zamiast tego
-     * returnuje string ktory trzeba zapisać do pliku
+     * walic event shedule zróbny crona !
      */
-    public function dump(){
-        $con = mysqli_connect ( "localhost", "root", "", "dslusarz_baza" );
-        $tables = array ();
-        $views = array();
-        $result = mysqli_query ( $con, 'SHOW FULL TABLES' ) or die(mysqli_error($con));
-        while ( $row = mysqli_fetch_array ( $result ) ) {
-            if($row[1] == "VIEW")
-                $views [] = $row [0];
-            else
-                $tables [] = $row [0];
-        }
-        // cycle through the tables
-        foreach ( $tables as $table ) {
-            $result = mysqli_query ( $con, "SELECT * FROM `$table`");
-            $num_fields = mysqli_num_fields ( $result );
-            $num_rows = mysqli_num_rows( $result );
-            $return .= "--\n-- Structure for the table $table\n--\n\n";
-            $return .= "DROP TABLE IF EXISTS `$table`;";
-            $row2 = mysqli_fetch_array ( mysqli_query ( $con, "SHOW CREATE TABLE `$table`" ) );
-            $return .= "\n\n" . $row2 [1] . ";\n\n";
-            if ($num_rows > 0) {
-                $return .= "--\n-- Data dump for the table $table\n--\n\n";
-            }
-            
-            $i = 0;
-            while ( $row = mysqli_fetch_array ( $result ) ) {
-                if ($i == 0) {
-                    $return .= "INSERT INTO `$table` VALUES\n";
-                }
-                $i++;
-                for($j = 0; $j < $num_fields; $j ++) {
-                    if ($j == 0) {
-                        $return .= '(';
-                    }
-                    $row [$j] = addslashes ( $row [$j] );
-                    $row [$j] = mysqli_real_escape_string ( $con, $row [$j] );
-                    if (isset ( $row [$j] )) {
-                        $return .= '"' . $row [$j] . '"';
-                    } 
-                    else {
-                        $return .= '""';
-                    }
-                    if ($j < ($num_fields - 1)) {
-                        $return .= ',';
-                    }
-                }
-                if ($i < $num_rows) { 
-                    $return .= "),\n"; 
-                } 
-                else { 
-                    $return .= ");\n\n"; 
-                }
-            }
-        }
-        foreach ( $views as $view ) {
-            $result = mysqli_query ( $con, "SELECT * FROM `$view`");
-            $num_fields = mysqli_num_fields ( $result );
-            $num_rows = mysqli_num_rows( $result );
-            $return .= "--\n-- Structure for the table $view\n--\n\n";
-            $return .= "DROP TABLE IF EXISTS `$view`;";
-            $row2 = mysqli_fetch_array ( mysqli_query ( $con, "SHOW CREATE TABLE `$view`" ) );
-            $return .= "\n\n" . $row2 [1] . ";\n\n";
-        }
-        return $return; 
+    public function setCron($path){
+         $exec = '59 23 01 * * root /usr/bin/php '.$path.'/backup.php';
+         return $exec;
     }
     
     /*
@@ -175,23 +116,78 @@ class Backup{
         }
         
     }
+    
+    /*
+     * jeżeli to cholerstwo myslqdump nie będzie działac to tu jest coś zamiast tego
+     * zapisuje to do pliku 
+     */
+    public function dump(){
+        $con = mysqli_connect ( "localhost", $this->userName, $this->password, $this->dataBase );
+        // cycle through the tables
+        foreach ( $this->tables as $table ) {
+            $result = mysqli_query ( $con, "SELECT * FROM `$table`");
+            $num_fields = mysqli_num_fields ( $result );
+            $num_rows = mysqli_num_rows( $result );
+            $return .= "--\n-- Structure for the table $table\n--\n\n";
+            $return .= "DROP TABLE IF EXISTS `$table`;";
+            $row2 = mysqli_fetch_array ( mysqli_query ( $con, "SHOW CREATE TABLE `$table`" ) );
+            $return .= "\n\n" . $row2 [1] . ";\n\n";
+            if ($num_rows > 0) {
+                $return .= "--\n-- Data dump for the table $table\n--\n\n";
+            }
+            
+            $i = 0;
+            while ( $row = mysqli_fetch_array ( $result ) ) {
+                if ($i == 0) {
+                    $return .= "INSERT INTO `$table` VALUES\n";
+                }
+                $i++;
+                for($j = 0; $j < $num_fields; $j ++) {
+                    if ($j == 0) {
+                        $return .= '(';
+                    }
+                    $row [$j] = addslashes ( $row [$j] );
+                    $row [$j] = mysqli_real_escape_string ( $con, $row [$j] );
+                    if (isset ( $row [$j] )) {
+                        $return .= '"' . $row [$j] . '"';
+                    } 
+                    else {
+                        $return .= '""';
+                    }
+                    if ($j < ($num_fields - 1)) {
+                        $return .= ',';
+                    }
+                }
+                if ($i < $num_rows) { 
+                    $return .= "),\n"; 
+                } 
+                else { 
+                    $return .= ");\n\n"; 
+                }
+            }
+        }
+        foreach ( $this->views as $view ) {
+            $result = mysqli_query ( $con, "SELECT * FROM `$view`");
+            $num_fields = mysqli_num_fields ( $result );
+            $num_rows = mysqli_num_rows( $result );
+            $return .= "--\n-- Structure for the table $view\n--\n\n";
+            $return .= "DROP TABLE IF EXISTS `$view`;";
+            $row2 = mysqli_fetch_array ( mysqli_query ( $con, "SHOW CREATE TABLE `$view`" ) );
+            $return .= "\n\n" . $row2 [1] . ";\n\n";
+        }
+        $file = $this->path.'backup_'.date("Y_m_d").'.sql';
+        $fp = fopen($file, "a");
+        flock($fp, 2);
+        fwrite($fp, $return);
+        flock($fp, 3);
+        fclose($fp); 
+        return $return; 
+    }
+    
+    public function recoverDataBase($date){
+        $query = "";
+        $query = $query.fread(fopen($this->path.'backup_'.$date.'.sql', "r"), filesize($this->path.'backup_'.$date.'.sql'));
+        return $query;
+    }
 }
-
-$controller = new Controller();
-$test = new Backup("C:/WebServ/httpd-users/dominik/Library/Backup/", "dslusarz_baza", "root", "");
-echo $test->toString();
-echo "<br>";
-echo $test->createEventBackup("10 WEEK", array("admins"));
-echo "<br>";
-echo $test->createEventBackup("10 WEEK");
-echo "<br>";
-$controller->doQuery($test->createEventBackup("10 WEEK"));
-
-echo "<br>";
-echo "<br>";
-echo $test->doBackup();
-
-echo "<br>";
-echo "<br>";
-echo $test->dump();
 
